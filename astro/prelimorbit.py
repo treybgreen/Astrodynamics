2da68bd5f786e2eff97901f15a75b2cdc30fedbe
Green, Trey B <treybgreenmn24@gmail.com>
2020-02-20 13:57:45 -0600

    Add in Gibbs' Method of Preliminary Orbit Determination

--- astro/prelimorbit.py
+++ astro/prelimorbit.py
@@ -5,14 +5,55 @@ class Gibbs:
     def __init__(self, mu=398600):
         self.mu = mu
 
-    def solve(self, r1, r2, r3):
+    def solve(self, r1, r2, r3, test=True):
         r1_mag = magnitude(r1)
         r2_mag = magnitude(r2)
         r3_mag = magnitude(r3)
 
-        n1_hat = r1 / r1_mag
+        c12 = cross_product(r1, r2)
+        c23 = cross_product(r2, r3)
+        c31 = cross_product(r3, r1)
 
-        v2 = [0, 0, 0]
+        if test is True:
+            norm1 = normalize(r1, mag=r1_mag)
+            norm2 = normalize(r2, mag=r2_mag)
+            norm3 = normalize(r3, mag=r3_mag)
+            u12 = [x / magnitude(c12) for x in c12]
+            u23 = [x / magnitude(c23) for x in c23]
+            u13 = [x / magnitude(c31) for x in c31]
+
+            d1 = dot_product(u12, norm3)
+            d2 = dot_product(u23, norm1)
+            d3 = dot_product(u13, norm2)
+
+            print("Verify Values are about 0:")
+            print(d1)
+            print(d2)
+            print(d3)
+            print("End Verify")
+
+        n1 = [r1_mag * x for x in c23]
+        n2 = [r2_mag * x for x in c31]
+        n3 = [r3_mag * x for x in c12]
+        numerator = [n1[0] + n2[0] + n3[0], n1[1] + n2[1] + n3[1], n1[2] + n2[2] + n3[2]]
+        numerator_mag = magnitude(numerator)
+
+        denominator = [c12[0] + c23[0] + c31[0], c12[1] + c23[1] + c31[1], c12[2] + c23[2] + c31[2]]
+        denominator_mag = magnitude(denominator)
+
+        s1 = r1[0] * (r2_mag - r3_mag) + r2[0] * (r3_mag - r1_mag) + r3[0] * (r1_mag - r2_mag)
+        s2 = r1[1] * (r2_mag - r3_mag) + r2[1] * (r3_mag - r1_mag) + r3[1] * (r1_mag - r2_mag)
+        s3 = r1[2] * (r2_mag - r3_mag) + r2[2] * (r3_mag - r1_mag) + r3[2] * (r1_mag - r2_mag)
+        s = [s1, s2, s3]
+        s_mag = magnitude(s)
+
+        multiple = math.sqrt(self.mu / (numerator_mag * denominator_mag))
+
+        cdr2 = cross_product(denominator, r2)
+        cdr2_r2 = [x / r2_mag for x in cdr2]
+        v_over_hm = [cdr2_r2[0] + s[0], cdr2_r2[1] + s[1], cdr2_r2[2] + s[2]]
+
+        v2 = [multiple * x for x in v_over_hm]
         return v2
 
 
--- astro/util.py
+++ astro/util.py
@@ -10,6 +10,27 @@ def magnitude(vector):
     return math.sqrt(mag_square)
 
 
+def normalize(vector, mag=None):
+    new_vector = [0, 0, 0]
+    if mag is None:
+        mag = magnitude(vector)
+    for i in range(len(vector)):
+        new_vector[i] = vector[i] / mag
+    return new_vector
+
+
+def cross_product(v1, v2):
+    vec = [v1[1] * v2[2] - v1[2] * v2[1], v1[2] * v2[0] - v1[0] * v2[2], v1[0] * v2[1] - v1[1] * v2[0]]
+    return vec
+
+
+def dot_product(v1, v2):
+    value = 0
+    for i in range(len(v1)):
+        value += v1[i] * v2[i]
+    return value
+
+
 def stumpff_s(z):
     # Stumpff S function expansion
     s_n = 0
